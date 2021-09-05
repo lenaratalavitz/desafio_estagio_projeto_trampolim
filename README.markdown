460c203cce149ebb555a9047718490eec1dce041
lenara <lenaratalavitz@gmail.com>
2021-09-05 17:03:22 -0300

    primeira atualização

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Teste HTML + CSS
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/lenaratalavitz/pen/XWgXzom](https://codepen.io/lenaratalavitz/pen/XWgXzom).
+
+
---
+++ dist/script.js
@@ -0,0 +1,10 @@
+const km = 5;
+const metro = km * 1000;
+
+console.log(`${metro}m é equivalente a ${km}km`);
+
+const celcius = 36.3;
+const Farht = celcius * 1.8 + 32;
+
+console.log(`${celcius}ºC é equivalente a ${Farht}ºF`);
+// para fazer esse cálculo pesquisei no google como fazia a conversão de Celsius para Fahrenheit
